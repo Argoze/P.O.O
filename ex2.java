e93db8c1d4f797c554c6ecf4f9a1d775abc7039a
Argoze <gustavoargozelopes@gmail.com>
2023-08-28 13:14:41 -0300

    first commit

---
+++ ex2.java
@@ -0,0 +1,59 @@
+
+package lista_de_ex1;
+
+import java.util.Scanner;
+
+
+public class ex2 {
+
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);      
+        int voto1 = 0;
+        int voto2 =0 ;
+        int voto3 = 0;
+        int voto4 = 0;
+        int votosnulos = 0;
+        int votosembranco = 0;
+        
+        System.out.println("figitr o numero do seu candidato entre 1 e 4, 5 para nulo e 6 para branco ");
+        
+        while (true) {
+            int voto = scanner.nextInt();
+            
+            if (voto == 0) {
+                break; 
+            }
+            
+            switch (voto) {
+                case 1: voto1++;
+                break;
+                case 2: voto2++;
+                break;
+                case 3: voto3++;
+                break;
+                case 4: voto4++;
+                    break;
+                case 5:
+                    votosnulos++;
+                    break;
+                case 6:
+                    votosembranco++;
+                    break;
+                default:
+                    System.out.println("nao tem esse candidato.");
+            }
+        }
+        
+        System.out.println("Total de votos para cada candidato:");
+        
+            System.out.println("votos do 1 candidato:"+ voto1);
+            System.out.println("votos do 2 candidato:"+ voto2);
+            System.out.println("votos do 3 candidato:"+ voto3);
+            System.out.println("votos do 4 candidato:"+ voto4);
+            System.out.println("votos nulos: " + votosnulos);
+            System.out.println("votos em branco: " + votosembranco);
+        
+        scanner.close();
+    }  
+    }
---
+++ ex8.java
@@ -0,0 +1,66 @@
+
+package lista_de_ex1;
+
+import java.util.Scanner;
+
+
+public class ex8 {
+
+    
+    public static void main(String[] args) {
+        
+  Scanner scanner = new Scanner(System.in);
+
+        int thabitantes = 1000;
+        int tmulheres = 0;
+        int thomens = 0;
+        int tde18a35 = 0;
+        int somagrupo = 0;
+        double samulher = 0;
+        double sahomem = 0;
+
+        for (int i = 0; i < thabitantes; i++) {
+            System.out.print("qual o sexo(0-feminino, 1-masculino) digite negativo para sair: ");
+            int sexo = scanner.nextInt();
+            if (sexo < 0) {
+                System.out.println("encerrado.");
+                break;
+            }
+            
+            System.out.print("qual a idade: ");
+            int idade = scanner.nextInt();
+             
+            
+            
+            System.out.print("qual a altura: ");
+            double altura = scanner.nextDouble();
+            
+            somagrupo += idade;
+
+            if (sexo == 0) {
+                samulher += altura;
+                tmulheres++;
+            } else {
+                sahomem += idade;
+                thomens++;
+            }
+
+            if (idade >= 18 && idade <= 35) {
+                tde18a35++;
+            }
+        }
+
+        double migrupo = (double) somagrupo / thabitantes;
+        double mamulher = tmulheres > 0 ? samulher / tmulheres : 0;
+        double mihomem = thomens > 0 ? sahomem / thomens : 0;
+        double pessoade18a35 = (double) tde18a35 / thabitantes * 100;
+
+        System.out.println("\nresultado:");
+        System.out.println("media da idade: " + migrupo);
+        System.out.println("media da altura das mulheres: " + mamulher);
+        System.out.println("media da idade dos homens: " + mihomem);
+        System.out.println("porcentagem de pessoas com 18 e 35 anos: " + pessoade18a35 + "%");
+
+        scanner.close();
+    }
+}
